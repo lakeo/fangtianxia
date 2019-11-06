13289f021bf8619f82443c62130c4144d6591678
xiaolu <blue.xiaolu@gmail.com>
2019-11-06 12:38:57 +0800

    fix

--- fangtianxia/pipelines.py
+++ fangtianxia/pipelines.py
@@ -4,20 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy.exporters import JsonLinesItemExporter
+from scrapy.exporters import JsonLinesItemExporter, CsvItemExporter
+
 
 class FangtianxiaPipeline(object):
     def __init__(self):
         self.newhouse_fp = open('newhouse.json', 'wb')
-        self.esfhouse_fp = open('esfhouse.json', 'wb')
-        self.newhouse_exporter = JsonLinesItemExporter(self.newhouse_fp, ensure_ascii=False)
-        self.esfhouse_exporter = JsonLinesItemExporter(self.esfhouse_fp, ensure_ascii=False)
+        self.newhouse_fp_csv = open('newhouse.csv', 'wb')
+        self.newhouse_exporter_json = JsonLinesItemExporter(self.newhouse_fp, ensure_ascii=False)
+        self.newhouse_exporter_csv = CsvItemExporter(self.newhouse_fp_csv)
 
     def process_item(self, item, spider):
-        self.newhouse_exporter.export_item(item)
-        self.esfhouse_exporter.export_item(item)
+        self.newhouse_exporter_json.export_item(item)
+        self.newhouse_exporter_csv.export_item(item)
         return item
 
     def close_spider(self, spider):
         self.newhouse_fp.close()
-        self.esfhouse_fp.close()
--- fangtianxia/spiders/ftx.py
+++ fangtianxia/spiders/ftx.py
@@ -3,6 +3,7 @@ import scrapy
 import re
 from fangtianxia.items import NewHouseItem, ESFHouseItem
 
+
 class FtxSpider(scrapy.Spider):
     name = 'ftx'
     allowed_domains = ['fang.com']
@@ -31,37 +32,19 @@ class FtxSpider(scrapy.Spider):
                 domain = url_module[1]
                 if 'bj.' in domain:
                     newhouse_url = ' http://newhouse.fang.com/house/s/'
-                    esf_url = ' http://esf.fang.com/'
                 else:
                     newhouse_url = scheme + "//" + "newhouse." + domain + "house/s/"
-                    esf_url = scheme + "//" + "esf." + domain
-                # yield scrapy.Request(url=newhouse_url, callback=self.parse_newhouse,
-                #                      meta={"info": (province, city)} )
-                yield scrapy.Request(url=esf_url, callback=self.parse_esf,
+                yield scrapy.Request(url=newhouse_url, callback=self.parse_newhouse,
                                      meta={"info": (province, city)})
 
     def parse_newhouse(self, response):
         province, city = response.meta.get('info')
         lis = response.xpath("//div[contains(@class,'nl_con')]/ul/li")
         for li in lis:
-            name = li.xpath(".//div[@class='nlcd_name']/a/text()").get().strip()
-            house_type_list = li.xpath(".//div[contains(@class,'house_type')]/a/text()").getall()
-            house_type_list = list(map(lambda x: re.sub(r"\s", "", x), house_type_list))
-            rooms = list(filter(lambda x: x.endswith("居"), house_type_list))
-            area = "".join(li.xpath(".//div[contains(@class,'house_type')]/text()").getall())
-            area = re.sub(r"\s|-|/", "", area)
-            address = li.xpath(".//div[@class='address']/a/@title").get()
-            district_text = "".join(li.xpath(".//div[@class='address']/a//text()").getall())
-            district = re.search(r".*\[(.+)\].*",district_text).group(1)
-            sale = li.xpath(".//div[contains(@class,'fangyuan')]/span/text()").get()
-            price = "".join(li.xpath(".//div[@class='nhouse_price']//text()").getall())
-            price = re.sub(r"\s|广告", "", price)
-            origin_url = li.xpath(".//div[@class='nlcd_name']/a/@href").get()
-            item = NewHouseItem(name=name, rooms=rooms,
-                area=area, address=address, district=district,
-                sale=sale, price=price, origin_url=origin_url,
-                province=province, city=city)
-            yield item
+            item_url = li.xpath(".//div[@class='nlcd_name']/a/@href").get() or ''
+            yield scrapy.Request(url=response.urljoin(item_url),
+                                 callback=self.parse_new_house_step1,
+                                 meta={"info": (province, city)})
 
         next_url = response.xpath("//div[@class='page']//a[@class='next']/@href").get()
         if next_url:
@@ -69,9 +52,70 @@ class FtxSpider(scrapy.Spider):
                                  callback=self.parse_newhouse,
                                  meta={"info": (province, city)})
 
+    def parse_new_house_step1(self, response):
+        province, city = response.meta.get('info')
+        url = response.xpath('//a[contains(text(), "楼盘详情")]/@href').get()
+        yield scrapy.Request(url=response.urljoin(url),
+                             callback=self.parse_new_house_step2,
+                             meta={"info": (province, city)})
+
+    def parse_str(self, _str):
+        if not _str:
+            return ''
+        _str = _str.replace('\n', '').replace('\t', '')
+        return _str
+
+    def parse_new_house_step2(self, response):
+        province, city = response.meta.get('info')
+        basic_div = response.xpath("//h3[contains(text(),'基本信息')]/parent::div")[0]
+        sale_div = response.xpath("//h3[contains(text(),'销售信息')]/parent::div")[0]
+
+        price = basic_div.xpath('.//div[@class="main-info-price"]/em/text()').get() or ''
+        name = response.xpath('//h1/a[@class="ts_linear"]/text()').get() or ''
+        wuyeleibie = basic_div.xpath('./ul/li[1]/div[2]/text()').get() or ''
+        xiangmutese = basic_div.xpath('./ul/li[2]/div[2]/span/text()').get() or ''
+        jianzhuleibie = basic_div.xpath('.//span[@class="bulid-type"]/text()').get() or ''
+        zhuangxiuzhuangkuang = basic_div.xpath('./ul/li/div/li[2]/div[2]/text()').get() or ''
+        chanquannianxian = basic_div.xpath('./ul/li/div/li[3]/div[2]/div/p/text()').get() or ''
+        huanxianweizhi = basic_div.xpath('./ul/li/div/li[4]/div[2]/text()').get() or ''
+        kaifashang = basic_div.xpath('./ul/li/div/li[5]/div[2]/a/text()').get() or ''
+        loupandizhi = basic_div.xpath('./ul/li/div/li[6]/div[2]/text()').get() or ''
+
+        xiaoshouzhuangtai = sale_div.xpath('.//ul/li[1]/div[2]/text()').get() or ''
+        loupanyouhui = sale_div.xpath('.//ul/li[2]/div[2]/text()').get() or ''
+        kaipanshijian = sale_div.xpath('.//ul/li[3]/div[2]/text()').get() or ''
+        jiaofangshijian = sale_div.xpath('.//ul/li[4]/div[2]/text()').get() or ''
+        shouloudizhi = sale_div.xpath('.//ul/li[5]/div[2]/text()').get() or ''
+        zixundianhua = sale_div.xpath('.//ul/li[6]/div[2]/text()').get() or ''
+        zhulihuxing = sale_div.xpath('.//ul/li[7]/div[2]/a/text()').get() or ''
+        yushouxuke = sale_div.xpath('.//ul/li[8]/div[2]/text()').get() or ''
+
+        item = NewHouseItem(province=province, city=city)
+        item['price'] = self.parse_str(price)
+        item['name'] = self.parse_str(name)
+        item['wuyeleibie'] = self.parse_str(wuyeleibie)
+        item['xiangmutese'] = self.parse_str(xiangmutese)
+        item['jianzhuleibie'] = self.parse_str(jianzhuleibie)
+        item['zhuangxiuzhuangkuang'] = self.parse_str(zhuangxiuzhuangkuang)
+        item['chanquannianxian'] = self.parse_str(chanquannianxian)
+        item['huanxianweizhi'] = self.parse_str(huanxianweizhi)
+        item['kaifashang'] = self.parse_str(kaifashang)
+        item['loupandizhi'] = self.parse_str(loupandizhi)
+        item['xiaoshouzhuangtai'] = self.parse_str(xiaoshouzhuangtai)
+        item['loupanyouhui'] = self.parse_str(loupanyouhui)
+        item['kaipanshijian'] = self.parse_str(kaipanshijian)
+        item['jiaofangshijian'] = self.parse_str(jiaofangshijian)
+        item['shouloudizhi'] = self.parse_str(shouloudizhi)
+        item['ziundianhua'] = self.parse_str(zixundianhua)
+        item['zhulihuxing'] = self.parse_str(zhulihuxing)
+        item['yushouxuke'] = self.parse_str(yushouxuke)
+        item['url'] = self.parse_str(response.url)
+        yield item
+
     def parse_esf(self, response):
         province, city = response.meta.get('info')
         dls = response.xpath("//div[@class='houseList']/dl")
+
         for dl in dls:
             item = ESFHouseItem(province=province, city=city)
             item['name'] = dl.xpath(".//p[@class='mt10']/a/span/text()").get()
@@ -93,8 +137,9 @@ class FtxSpider(scrapy.Spider):
                 detail_url = dl.xpath(".//p[@class='title']/a/@href").get()
                 item['origin_url'] = response.urljoin(detail_url)
                 yield item
+
         next_url = response.xpath("//a[@id='PageControll_hlk_next']/@href").get()
         yield scrapy.Request(url=response.urljoin(next_url),
-                             callback=self.parse_esf,
-                             meta={"info": (province, city)}
-                             )
+                         callback=self.parse_esf,
+                         meta={"info": (province, city)}
+                         )
